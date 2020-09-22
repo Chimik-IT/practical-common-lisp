e932c8c2a1b4d87d26654c6f310301ed5785558c
Tobias Speer <speer@lansol.de>
2020-09-22 15:43:29 +0200

    documentation added

--- zfs-calc.lisp
+++ zfs-calc.lisp
@@ -1,3 +1,14 @@
+;; Name: ZFS-Calculator
+;; Version: 0.0.1
+;; Author: Chimik-IT
+;; Date: 09/22/2020
+
+;; In this early stage I have created one function, that resembles the notion of how raid5 and raid6 were
+;; calculated. with 1-1/n and 1-2/n respactively while n is the amount of disks used in the arrays. I added
+;; 1-3/n for the raidz3 serving triple parity. Since the ZFS does a little slop allocation I needed to add
+;; the correction factor "slop" it's bound to some conditionals marking it's size borders.
+
+
 (defvar number-of-discs)
 (defvar size-of-discs)
 (defvar zfs-raidz-number)
