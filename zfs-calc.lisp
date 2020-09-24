635e4f8b3f024fa19cf4d0d305fef479dca07df1
Tobias Speer <speer@lansol.de>
2020-09-24 17:57:43 +0200

    no need for units yet

--- zfs-calc.lisp
+++ zfs-calc.lisp
@@ -15,21 +15,6 @@
 (defvar disc-size-unit)
 (defvar slop-space-allocation)
 (defvar size-entry)
-(defvar binary-units '(("MB" 0)
-		       ("GB" 10)
-		       ("TB" 20)))
-(defvar decimal-units '(("MiB" 0)
-			("GiB" 3)
-			("TiB" 6)))
-
-(setq all-units (mapcar #'car (concatenate 'list binary-units decimal-units)))
-(defun concat-to-string (list)
-  (if (listp list)
-      (with-output-to-string (s)
-	(dolist (item list)
-	  (if (stringp item)
-	      (format s "~a " item))))))
-(concat-to-string all-units)
 (defun raidz-calc ()
   (princ "Enter disc count: ")
   (setq number-of-discs (read))
@@ -41,9 +26,7 @@
   (setq disc-size-unit (read))
   (cond ((> (/ (* size-of-discs number-of-discs) 32) (/ (* size-of-discs number-of-discs) 2)) (setq slop-space-allocation (/ (* size-of-discs number-of-discs) 2)))
 	(t (setq slop-space-allocation (/ (* size-of-discs number-of-discs) 32))))
-  (if (or (assoc disc-size-unit binary-units :test #'string-equal) (assoc disc-size-unit decimal-units :test #'string-equal))
-      (format t "The usable size of the ZFS array: ~F~A" (- (* number-of-discs size-of-discs (- 1 (/ zfs-raidz-number number-of-discs))) slop-space-allocation) disc-size-unit)
-  (format t "Please use one of the following unints: ~s" (concat-to-string all-units)))
+  (format t "The usable size of the ZFS array: ~F~A" (- (* number-of-discs size-of-discs (- 1 (/ zfs-raidz-number number-of-discs))) slop-space-allocation (parity-padding number-of-discs)) disc-size-unit)
   (setq number-of-discs nil
 	size-of-discs nil
 	zfs-raidz-number nil
